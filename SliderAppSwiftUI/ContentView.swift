7c1f16b5ac8a48b37eb911b6b10caa30d4051ce2
Bodikov Kuat <kuat.bodikov@gmail.com>
2022-02-27 17:34:20 +0500

    Initial Commit

---
+++ SliderAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SliderAppSwiftUI
+//
+//  Created by Kuat Bodikov on 27.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SliderAppSwiftUI/SliderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderAppSwiftUIApp.swift
+//  SliderAppSwiftUI
+//
+//  Created by Kuat Bodikov on 27.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
